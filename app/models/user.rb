5533ea750a6b62c8539a4bd2374bbace63723b15
Klymentinka <alena.norvatova@gmail.com>
2021-11-29 00:15:34 +0000

    W4D2_part2

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,15 @@
 class User < ActiveRecord::Base
 
+    # Data relationship
+    
     has_many :hamstagram_posts
     has_many :comments
     has_many :likes
+    
+    # Data validation
+
+    validates :email, :username, uniqueness: true
+    validates :email, :avatar_url, :username, :password, presence: true
+  
 
 end
